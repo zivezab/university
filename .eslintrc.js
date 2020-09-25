11be40a4f8dee3b1bc7977ed077742d3ab08d7cc
Zive Lai <zive.lai@Zives-MacBook-Pro.local>
2020-09-26 03:13:11 +0800

    update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,20 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true,
+  env: {
+    browser: true,
+    es2021: true,
   },
-  'extends': [
-    'plugin:react/recommended',
-    'google',
-  ],
-  'parserOptions': {
-    'ecmaFeatures': {
-      'jsx': true,
+  extends: ['plugin:react/recommended', 'google'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    'ecmaVersion': 12,
-    'sourceType': 'module',
+    ecmaVersion: 12,
+    sourceType: 'module',
   },
-  'plugins': [
-    'react',
-  ],
-  'rules': {
+  plugins: ['react'],
+  rules: {
+    'require-jsdoc': 0,
+    'react/prop-types': 0,
+    'max-len': ['error', {code: 120}],
   },
 };
